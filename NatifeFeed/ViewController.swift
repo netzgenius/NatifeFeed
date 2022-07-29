2b693369d6cd00f19f6198f54c4c8a02a9d5a5f8
Kirilo <fffyffff@gmail.com>
2022-07-29 04:11:08 +0300

    Initial Commit

---
+++ NatifeFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NatifeFeed
+//
+//  Created by Dasha Sharapova on 29/7/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
